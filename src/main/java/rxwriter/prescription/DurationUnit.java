6a32bf4604a3e83bbc7cbde8b5c746510767283d
Jim Weaver <weaver.je@gmail.com>
2022-03-19 20:25:37 -0500

    initial commit

---
+++ src/main/java/rxwriter/prescription/DurationUnit.java
@@ -0,0 +1,5 @@
+package rxwriter.prescription;
+
+public enum DurationUnit {
+
+}
---
+++ src/test/java/rxwriter/prescription/DurationUnitTest.java
@@ -0,0 +1,7 @@
+package rxwriter.prescription;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DurationUnitTest {
+
+}
